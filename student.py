67ac900508316099ff44946d1e712b4c26a3b98f
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2020-07-26 15:01:53 -0500

    First commit

---
+++ student.py
@@ -0,0 +1,52 @@
+# Linear Regression Algorithm
+import pandas as pd
+import numpy as np
+import sklearn
+# from sklearn import linear_model
+# from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
+# Data
+data = pd.read_csv("student-mat.csv", sep=";")
+
+# Training
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+X = np.array(data.drop([predict], 1))
+Y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+
+best = 0
+"""for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+    # Create a model
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+    if acc > best:
+        best = acc
+        with open("studentModel.pickle", "wb") as f:
+            pickle.dump(linear, f)"""
+
+pickle_in = open("studentModel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print("Co:\n", linear.coef_)
+print("Intercept:\n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+p = "G2"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
